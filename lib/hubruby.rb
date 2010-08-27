1525462c1eda4e91e263becd3d103fa78327bffb
Diógenes <diogenes.araujo@gmail.com>
2010-08-27 00:21:21 -0300

    Loading dependencies

--- lib/hubruby.rb
+++ lib/hubruby.rb
@@ -6,4 +6,16 @@ require 'httparty'
 
 module Hubruby
   VERSION = '0.0.1'
+
+  def self.require_all
+    Dir[File.join(File.dirname(__FILE__), %W(github ** *.rb))].each do |f|
+      require f
+    end
+  end
+
+  def self.init
+    require_all
+  end
 end
+
+Hubruby.init
