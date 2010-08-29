e63f452e9fcd8fa5fc32c6a28e8d89fb919faeb2
Diógenes <diogenes.araujo@gmail.com>
2010-08-29 12:56:16 -0300

    Add method to mapping of a User collection from a JSON response (User#users_from_json)

--- lib/github/models/user.rb
+++ lib/github/models/user.rb
@@ -3,7 +3,13 @@ require 'ostruct'
 module GitHub
   class User < OpenStruct
     def self.from_json(json)
-      new(json['user'])
+      new(json)
+    end
+
+    def self.users_from_json(json)
+      json.inject([]) do |users, username|
+        users << from_json(:username => username)
+      end
     end
   end
 end
