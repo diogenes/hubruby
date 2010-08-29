a7fdfda1f6fe43bcea8e11f04173fdd9ced5cfa7
Diógenes <diogenes.araujo@gmail.com>
2010-08-29 13:06:21 -0300

    Add method to map a collection of repositories from a JSON response

--- lib/github/models/repository.rb
+++ lib/github/models/repository.rb
@@ -5,5 +5,11 @@ module GitHub
     def self.from_json(json)
       new(json)
     end
+
+    def self.repositories_from_json(json)
+      json.inject([]) do |repositories, repository_json|
+        repositories << from_json(repository_json)
+      end
+    end
   end
 end
